2c7d643b3c3fd20a9401c95dbb68341967ea8a56
Diego Negrette <diegonegrette42@gmail.com>
2022-03-15 02:11:26 -0400

    Added create tag feature

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
-        time.sleep(5)
+        time.sleep(1)
         db_conn = None
         while not db_conn:
             try:
